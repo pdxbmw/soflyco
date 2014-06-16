1c1f19a1a404575127d6f85d2b3a4918e37ea7ba
Andi <andi.greisel@cbs.com>
2014-06-16 14:18:41 -0700

    Force deploy.

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from sofly import create_app
 
 import os
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default') 
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')  
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
